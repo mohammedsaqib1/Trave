a5887003fe447f748b2241d4516289ad3b117fee
Saqib Ansari <saqibansari.prime@gmail.com>
2021-10-12 15:22:49 +0530

    completed homescreen rooms showing part

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,7 +3,22 @@ import React from 'react'
 function Room({room}) {
     return (
         <div>
-            
+            <div className="row boxshadow">
+                <div className="col-md-4">
+                    <img src={room.imageurls[0]} className="smallimg" />
+                </div>
+                <div className="col-md-7 roomproperties">
+                    <b>
+                    <h1>{room.name}</h1>
+                    <p>Max count : {room.maxcount}</p>
+                    <p>Phone Number : {room.phonenumber}</p>
+                    <p>Type : {room.type}</p>
+                    </b>
+                    <div style={{float :'right'}}>
+                        <button className="btn btn-gradient btn-lg">View Details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -21,15 +21,15 @@ function Homescreen() {
   }, []);
 
   return (
-    <div>
-      <div className="row">
+    <div className="container">
+      <div className="row justify-content-center mt-5">
         {loading ? (
           <h1>loading....</h1>
         ) : error ? (
           <h1>Error</h1>
         ) : (
           rooms.map((room) => {
-            return <div className="col-md-9">
+            return <div className="col-md-9 mt-2">
                 <Room room={room}/>
             </div>;
           })
