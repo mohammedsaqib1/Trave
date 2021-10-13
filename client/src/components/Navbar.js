43f67669ff6bcf5a52f3c330cdf758129282cd1e
Saqib Ansari <saqibansari.prime@gmail.com>
2021-10-14 00:39:00 +0530

    user authentication completed with navbar login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import imagelogo from "../images/logo.png";
 function Navbar() {
+  const user = JSON.parse(localStorage.getItem('currentUser'));
+  function logout(){
+    localStorage.removeItem('currentUser')
+    window.location.href='/login'
+  }
   return (
     <div>
       <nav class="navbar header">
@@ -8,7 +13,7 @@ function Navbar() {
           <img src={imagelogo} alt="logo" />
         </a>
         <nav class="navbar navbar-expand-lg">
-          {/* <button
+            <button
             class="navbar-toggler"
             type="button"
             data-toggle="collapse"
@@ -17,20 +22,33 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
-          </button> */}
+            <span class="navbar-toggler-icon" > <i class="fa fa-bars" style={{color: 'white'}} ></i> </span>
+          
+          </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/register">
-                  Register
-                </a>
-              </li>
+            <ul class="navbar-nav mr-5">
+            {user ? (<>
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-user mr-2"></i>{user.name}
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Bookings</a>
+                    <a class="dropdown-item" href="#" onClick={logout}>Logout</a>
+                  </div>
+                </div>
+              </>) : (<>
+                <li class="nav-item">
+                  <a class="nav-link" href="/login">
+                    Login
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/register">
+                    Register
+                  </a>
+                </li>
+              </>)}
             </ul>
           </div>
         </nav>
